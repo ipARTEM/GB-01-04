45fadd7f4e2e37beff63685ccb08311560f6ab81
Artem <a.khimin@yandex.ru>
2021-05-10 01:29:58 +0300

    Добавьте файлы проекта.

---
+++ GB-01-04/People.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_01_04
+{
+    class People
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public void GetFullName(string firstName, string lastName, string patronymic)
+        {
+            Console.WriteLine($" Имя: {firstName}, Фамилия: {lastName}, Отчество: {patronymic}");
+
+        }
+
+        public void GetFullName()
+        {
+            Console.WriteLine($" Имя: {FirstName}, Фамилия: {LastName}, Отчество: {Patronymic}");
+
+        }
+
+    }
+}
---
+++ GB-01-04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_01_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            People people = new People();
+
+            Console.WriteLine("Введите Имя: ");
+            people.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Введите Фамилию: ");
+            people.LastName = Console.ReadLine();
+
+            Console.WriteLine("Введите Отчество: ");
+            people.Patronymic = Console.ReadLine();
+
+            people.GetFullName();
+
+
+        }
+    }
+}
